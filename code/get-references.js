b0828c5b63b645f05ac70f9ccd62d82ac34a7626
Robinson Farrar <robinson.farrar@gmail.com>
2023-02-16 15:48:24 -0500

    trying query based get_references

--- code/get-references.js
+++ code/get-references.js
@@ -31,7 +31,7 @@ const refs = {
 			this.crawl(child, {namespace: refs, self, attr, array_attr, id_stop});
 		}
 		if (!parent.hasAttribute(attr) || (id_stop && parent.hasAttribute('id'))) {
-			return container;
+			return namespace;
 		}
 		let name = parent.getAttribute(attr);
 		refs = Object.keys(refs).length > 1 ? refs : parent;
@@ -45,4 +45,37 @@ const refs = {
 		}
 		return namespace;
 	},
+	/*
+	query_all({namespace=this, self='self', attr='js-ref', array_attr='is-array'}={}) {
+		const selector = `[${attr}]`;
+		let tree = new Map();
+		let root = {};
+		const insert = (obj, name, element) => {
+			if (!element.hasAttribute(array_attr)) {
+				obj[name] = element;
+			} else if (!(name in obj)) {
+				obj[name] = [element];
+			} else {
+				obj[name].push(element);
+			}
+		}
+		for (const element of document.querySelectorAll(selector)) {
+			const name = element.getAttribute(attr);
+			const parent = element.closest(selector);
+			if (parent == null) {
+				root[name] = element;
+				continue;
+			}
+			if (!tree.has(parent)) {
+				tree.set(parent, {self: parent});
+			}
+			insert(tree.get(parent), name, element);
+		}
+		for (const name in root) {
+			const element = root[name];
+			const value = tree.has(element) ? tree.get(element) : element;
+			insert(namespace, name, value);
+		}
+	},
+	*/
 };
